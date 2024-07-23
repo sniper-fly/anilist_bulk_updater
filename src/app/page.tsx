4d5b4f4da34c54eb542b228028fe4e3fa32a713f
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-07-24 00:28:47 +0900

    refactor button click handler on top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-'use client'
+"use client";
 
 import updater from "./updater";
 
 export default function Home() {
-  // const { data } = await getClient().query({
-  //   query: TEST_QUERY,
-  // });
-  // const entries = data?.MediaListCollection?.lists?.[0]?.entries
-  // if (!entries) return "";
-  // const titles = entries.map((entry) => entry?.media?.title?.native);
+  function handleClick() {
+    (async () => {
+      await updater();
+    })();
+  }
 
-  // const medium = entries != undefined ? Array.from(entries) : null;
-  return <main>
-    <h1>AniList Updater</h1>
-    <button onClick={ () => { updater(); }} >Update</button>
-  </main>;
+  return (
+    <main>
+      <h1>AniList Updater</h1>
+      <button onClick={handleClick}>Update</button>
+    </main>
+  );
 }
