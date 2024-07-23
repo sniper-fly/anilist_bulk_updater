dfecd29a3295913c014e17c0d4eb784321bb0345
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-07-23 17:51:39 +0900

    fix remote url

--- src/ApolloClient.ts
+++ src/ApolloClient.ts
@@ -10,7 +10,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
     cache: new InMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: "http://example.com/api/graphql",
+      uri: "https://graphql.anilist.co",
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       // fetchOptions: { cache: "no-store" },
