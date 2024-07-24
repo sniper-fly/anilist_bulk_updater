8a9c1c45af87656a36589d0ae5e8e7066ba3bcd0
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-07-24 11:20:36 +0900

    implement graphql client fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,44 @@
+"use client";
+
+import { gql } from "@/graphql";
 import UpdateButton from "./updateButton";
+import { useQuery } from "@apollo/client";
+
+const LIST_POPULAR_ANIME = gql(`
+  query LIST_POPULAR_ANIME($page: Int) {
+    Page(page: $page, perPage: 50) {
+      media(sort: POPULARITY_DESC) {
+        id
+        title {
+          native
+        }
+      }
+    }
+  }
+`);
 
 export default function Home() {
+  const { loading, error, data } = useQuery(LIST_POPULAR_ANIME, {
+    variables: { page: 1 },
+  });
+
+  if (loading) return <p>Loading...</p>;
+
+  const medium = data?.Page?.media;
+
+  if (error || !medium) return <p>Error</p>;
+
+  const animeInfo = []
+  for (const media of medium) {
+    animeInfo.push({
+      id: media?.id,
+      title: media?.title?.native,
+    });
+  }
+
+  console.log(data)
+  console.log(animeInfo)
+
   return (
     <main>
       <h1>AniList Updater</h1>
--- src/graphql/gql.ts
+++ src/graphql/gql.ts
@@ -13,6 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
+    "\n  query LIST_POPULAR_ANIME($page: Int) {\n    Page(page: $page, perPage: 50) {\n      media(sort: POPULARITY_DESC) {\n        id\n        title {\n          native\n        }\n      }\n    }\n  }\n": types.List_Popular_AnimeDocument,
     " \n  mutation SAVE_MEDIA_QUERY {\n    SaveMediaListEntry(mediaId:1) {\n      media {\n        title {\n          native\n        }\n      }\n    }\n  }\n": types.Save_Media_QueryDocument,
 };
 
@@ -30,6 +31,10 @@ const documents = {
  */
 export function gql(source: string): unknown;
 
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  query LIST_POPULAR_ANIME($page: Int) {\n    Page(page: $page, perPage: 50) {\n      media(sort: POPULARITY_DESC) {\n        id\n        title {\n          native\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query LIST_POPULAR_ANIME($page: Int) {\n    Page(page: $page, perPage: 50) {\n      media(sort: POPULARITY_DESC) {\n        id\n        title {\n          native\n        }\n      }\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
